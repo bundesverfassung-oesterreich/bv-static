1c6e131539ba7b0a37645629dd88c447a9d5699e
cfhaak <cfhaak@users.noreply.github.com>
2023-05-10 12:49:56 +0000

    deploy: 0193e4b7a0d3598c3eabbf2f299ab6721cb28294

---
+++ js/scripts/fetch_editions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# # def vars
+temp_path="./tmp_tmp/"
+zip_file="${temp_path}main.zip"
+fetched_editions_path="${temp_path}bv-working-data-main/data/editions/"
+target_editions_path="./data/editions/"
+# # prepare folder structure
+rm -rf $temp_path
+mkdir $temp_path
+# # fetch data from repo, unzip, and move to destination
+wget https://github.com/bundesverfassung-oesterreich/bv-working-data/archive/refs/heads/main.zip -O $zip_file
+unzip $zip_file -d $temp_path
+# # check for target dir
+if [ ! -d "$target_editions_path" ]; then mkdir "$target_editions_path"; fi
+find $fetched_editions_path -name "*.xml" -exec cp "{}" "$target_editions_path" \;
+# # remove temp folder
+rm -rf $temp_path
---
+++ js/ts_search.js
@@ -0,0 +1,206 @@
+const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+  server: {
+    apiKey: "9KuQLxkcSSBjYobV50wKv6KBJFz9DzjO",
+    nodes: [
+      {
+        host: "typesense.acdh-dev.oeaw.ac.at",
+        port: "443",
+        protocol: "https",
+      },
+    ],
+  },
+  // The following parameters are directly passed to Typesense's search API endpoint.
+  //  So you can pass any parameters supported by the search endpoint below.
+  //  query_by is required.
+  //  filterBy is managed and overridden by InstantSearch.js. To set it, you want to use one of the filter widgets like refinementList or use the `configure` widget.
+  additionalSearchParameters: {
+    query_by: "full_text,title",
+  },
+});
+
+const searchClient = typesenseInstantsearchAdapter.searchClient;
+const search = instantsearch({
+  searchClient,
+  indexName: "bundes_verfassung_oesterreich",
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: "#searchbox",
+    autofocus: true,
+    cssClasses: {
+      form: "form-inline",
+      input: "form-control col-md-11",
+      submit: "btn",
+      reset: "btn",
+    },
+  }),
+
+  instantsearch.widgets.hits({
+    container: "#hits",
+    templates: {
+      empty: "Keine Resultate für <q>{{ query }}</q>",
+      item: `
+              <h5><a href="{{ id }}">{{#helpers.snippet}}{ "attribute": "title", "highlightedTagName": "mark" }{{/helpers.snippet}}</a></h5>
+              <p style="overflow:hidden;max-height:210px;">{{#helpers.snippet}}{ "attribute": "full_text", "highlightedTagName": "mark" }{{/helpers.snippet}}</p>
+              <h5><span class="badge badge-primary">{{ project }}</span></h5>
+              <div>
+                  <div>
+                      {{#persons}}
+                      <span class="badge bg-secondary">{{ . }}</span>
+                      {{/persons}}
+                  </div>
+                  <div>
+                      {{#works}}
+                      <span class="badge bg-success">{{ . }}</span>
+                      {{/works}}
+                  </div>
+                  <div>
+                      {{#places}}
+                      <span class="badge bg-info">{{ . }}</span>
+                      {{/places}}
+                  </div>
+              </div>
+          `,
+    },
+  }),
+
+  /*instantsearch.widgets.pagination({
+    container: "#pagination",
+  }),
+
+  instantsearch.widgets.stats({
+    container: "#stats-container",
+    templates: {
+      text: `
+            {{#areHitsSorted}}
+              {{#hasNoSortedResults}}keine Treffer{{/hasNoSortedResults}}
+              {{#hasOneSortedResults}}1 Treffer{{/hasOneSortedResults}}
+              {{#hasManySortedResults}}{{#helpers.formatNumber}}{{nbSortedHits}}{{/helpers.formatNumber}} Treffer {{/hasManySortedResults}}
+              aus {{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}}
+            {{/areHitsSorted}}
+            {{^areHitsSorted}}
+              {{#hasNoResults}}keine Treffer{{/hasNoResults}}
+              {{#hasOneResult}}1 Treffer{{/hasOneResult}}
+              {{#hasManyResults}}{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}} Treffer{{/hasManyResults}}
+            {{/areHitsSorted}}
+            gefunden in {{processingTimeMS}}ms
+          `,
+    },
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-places",
+    attribute: "places",
+    searchable: true,
+    searchablePlaceholder: "Suchen",
+    cssClasses: {
+      searchableInput: "form-control form-control-sm mb-2 border-light-2",
+      searchableSubmit: "d-none",
+      searchableReset: "d-none",
+      showMore: "btn btn-secondary btn-sm align-content-center",
+      list: "list-unstyled",
+      count: "badge ml-2 badge-info",
+      label: "d-flex align-items-center text-capitalize",
+      checkbox: "mr-2",
+    },
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-persons",
+    attribute: "persons",
+    searchable: true,
+    searchablePlaceholder: "Suchen",
+    cssClasses: {
+      searchableInput: "form-control form-control-sm mb-2 border-light-2",
+      searchableSubmit: "d-none",
+      searchableReset: "d-none",
+      showMore: "btn btn-secondary btn-sm align-content-center",
+      list: "list-unstyled",
+      count: "badge ml-2 badge-secondary",
+      label: "d-flex align-items-center text-capitalize",
+      checkbox: "mr-2",
+    },
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-works",
+    attribute: "works",
+    searchable: true,
+    searchablePlaceholder: "Suchen",
+    cssClasses: {
+      searchableInput: "form-control form-control-sm mb-2 border-light-2",
+      searchableSubmit: "d-none",
+      searchableReset: "d-none",
+      showMore: "btn btn-secondary btn-sm align-content-center",
+      list: "list-unstyled",
+      count: "badge ml-2 badge-success",
+      label: "d-flex align-items-center text-capitalize",
+      checkbox: "mr-2",
+    },
+  }),
+
+  instantsearch.widgets.rangeInput({
+    container: "#range-input",
+    attribute: "year",
+    templates: {
+      separatorText: "to",
+      submitText: "Suchen",
+    },
+    cssClasses: {
+      form: "form-inline",
+      input: "form-control",
+      submit: "btn",
+    },
+  }),
+
+  instantsearch.widgets.pagination({
+    container: "#pagination",
+    padding: 2,
+    cssClasses: {
+      list: "pagination",
+      item: "page-item",
+      link: "page-link",
+    },
+  }),
+
+  instantsearch.widgets.clearRefinements({
+    container: "#clear-refinements",
+    templates: {
+      resetLabel: "Filter zurücksetzen",
+    },
+    cssClasses: {
+      button: "btn",
+    },
+  }),
+
+  instantsearch.widgets.currentRefinements({
+    container: "#current-refinements",
+    cssClasses: {
+      delete: "btn",
+      label: "badge",
+    },
+  }),
+
+  instantsearch.widgets.sortBy({
+    container: "#sort-by",
+    items: [
+      { label: "Default", value: "bundes_verfassung_oesterreich" },
+      { label: "Jahr (asc)", value: "hsl/sort/date:asc" },
+      { label: "Jahr (desc)", value: "hsl/sort/date:desc" },
+    ],
+  }),
+
+  instantsearch.widgets.configure({
+    hitsPerPage: 8,
+    attributesToSnippet: ["full_text"],
+  }),*/
+]);
+
+// search.addWidgets([
+//   instantsearch.widgets.configure({
+//     attributesToSnippet: ["full_text:30", "title"],
+//   }),
+// ]);
+
+search.start();
