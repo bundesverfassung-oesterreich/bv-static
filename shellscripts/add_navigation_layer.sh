308cf96860a49dc46188ac1e11c3ed93c33ae89b
cfhaak <c.f.haak@posteo.de>
2023-07-21 09:39:58 +0200

    made script for nav layer more verbose, added check

--- shellscripts/add_navigation_layer.sh
+++ shellscripts/add_navigation_layer.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-sourcefiles="./data/editions/bv_doc_id__6.xml"
+sourcefiles="./data/editions/bv_doc_id__*.xml"
 xsl_script="./xslt/navigation_layer.xsl"
 for sourcefile in $sourcefiles
 do
     tempfile="$sourcefile.tmp"
+    echo "creating navigation anchor in $sourcefile"
     java -jar ./saxon/saxon9he.jar -o:$tempfile -s:$sourcefile $xsl_script
+    if [ $? != 0 ]; then
+        echo "error while creating navigation anchor in $sourcefile"
+        exit 1
+    fi
     mv $tempfile $sourcefile
 done
