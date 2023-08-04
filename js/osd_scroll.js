5a1f71ec26a8c542f7fa1c401f5db5e1eecc2fe4
cfhaak <cfhaak@users.noreply.github.com>
2023-08-04 11:55:40 +0000

    deploy: 9635bde72396490fd49d88a8dc5a1a622a3091f4

--- js/osd_scroll.js
+++ js/osd_scroll.js
@@ -5,19 +5,18 @@ var wrapper_column = document.getElementsByClassName("facsimiles")[0];
 var text_container = document.getElementById("section");
 // set osd wrapper container width
 if (text_container !== null) {
-    var width = text_container.clientWidth;
-}
-else {
-    var width = 0;
+  var width = text_container.clientWidth;
+} else {
+  var width = 0;
 }
 var container = document.getElementById("viewer");
 // check if facsimiles are displayed
 if (!wrapper_column.classList.contains("fade")) {
-    // if true get width from sibling container - offset
-    container.style.width = `${String(text_container.clientWidth - 25)}px`;
+  // if true get width from sibling container - offset
+  container.style.width = `${String(text_container.clientWidth - 25)}px`;
 } else {
-    // if false get width from sibling container / 2
-    container.style.width = `${String(text_container.clientWidth / 2)}px`;
+  // if false get width from sibling container / 2
+  container.style.width = `${String(text_container.clientWidth / 2)}px`;
 }
 
 /*
@@ -26,12 +25,12 @@ get all image urls stored in span el class tei-xml-images
 creates an array for osd viewer with static images
 ##################################################################
 */
-var element = document.getElementsByClassName('pb');
+var element = document.getElementsByClassName("pb");
 var tileSources = [];
 var img = element[0].getAttribute("source");
 var imageURL = {
-    type: 'image',
-    url: img
+  type: "image",
+  url: img,
 };
 tileSources.push(imageURL);
 
@@ -41,11 +40,12 @@ initialize osd
 ##################################################################
 */
 var viewer = OpenSeadragon({
-    id: 'container_facs_1',
-    prefixUrl: 'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/4.0.0/images/',
-    sequenceMode: true,
-    showNavigator: true,
-    tileSources: tileSources
+  id: "container_facs_1",
+  prefixUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/4.0.0/images/",
+  sequenceMode: true,
+  showNavigator: true,
+  tileSources: tileSources,
 });
 /*
 ##################################################################
@@ -72,26 +72,26 @@ viewport position of next and previous element with class pb
 pb = pagebreaks
 ##################################################################
 */
-window.addEventListener("scroll", function(event) {
-    // elements in view
-    var esiv = [];
-    for (let el of element) {
-        if (isInViewportAll(el)) {
-            esiv.push(el);
-        }
+window.addEventListener("scroll", function (event) {
+  // elements in view
+  var esiv = [];
+  for (let el of element) {
+    if (isInViewportAll(el)) {
+      esiv.push(el);
     }
-    if (esiv.length != 0) {
-        // first element in view
-        var eiv = esiv[0];
-        // get idx of element
-        var eiv_idx = Array.from(element).findIndex((el) => el === eiv);
-        idx = eiv_idx + 1;
-        prev_idx = eiv_idx - 1
-        // test if element is in viewport position to load correct image
-        if (isInViewport(element[eiv_idx])) {
-            loadNewImage(element[eiv_idx]);
-        }
+  }
+  if (esiv.length != 0) {
+    // first element in view
+    var eiv = esiv[0];
+    // get idx of element
+    var eiv_idx = Array.from(element).findIndex((el) => el === eiv);
+    idx = eiv_idx + 1;
+    prev_idx = eiv_idx - 1;
+    // test if element is in viewport position to load correct image
+    if (isInViewport(element[eiv_idx])) {
+      loadNewImage(element[eiv_idx]);
     }
+  }
 });
 
 /*
@@ -100,33 +100,33 @@ function to trigger image load and remove events
 ##################################################################
 */
 function loadNewImage(new_item) {
-    if (new_item) {
-        // source attribute hold image item id without url
-        var new_image = new_item.getAttribute("source");
-        var old_image = viewer.world.getItemAt(0);
-        if (old_image) {
-            // get url from current/old image and replace the image id with
-            // new id of image to be loaded
-            // access osd viewer and add simple image and remove current image
-            viewer.addSimpleImage({
-                url: new_image,
-                success: function(event) {
-                    function ready() {
-                        setTimeout(() => {
-                            viewer.world.removeItem(viewer.world.getItemAt(0));
-                        }, 200)
-                    }
-                    // test if item was loaded and trigger function to remove previous item
-                    if (event.item) {
-                        // .getFullyLoaded()
-                        ready();
-                    } else {
-                        event.item.addOnceHandler('fully-loaded-change', ready());
-                    }
-                }
-            });
-        }
+  if (new_item) {
+    // source attribute hold image item id without url
+    var new_image = new_item.getAttribute("source");
+    var old_image = viewer.world.getItemAt(0);
+    if (old_image) {
+      // get url from current/old image and replace the image id with
+      // new id of image to be loaded
+      // access osd viewer and add simple image and remove current image
+      viewer.addSimpleImage({
+        url: new_image,
+        success: function (event) {
+          function ready() {
+            setTimeout(() => {
+              viewer.world.removeItem(viewer.world.getItemAt(0));
+            }, 200);
+          }
+          // test if item was loaded and trigger function to remove previous item
+          if (event.item) {
+            // .getFullyLoaded()
+            ready();
+          } else {
+            event.item.addOnceHandler("fully-loaded-change", ready());
+          }
+        },
+      });
     }
+  }
 }
 
 /*
@@ -135,20 +135,20 @@ accesses osd viewer prev and next button to switch image and
 scrolls to next or prev span element with class pb (pagebreak)
 ##################################################################
 */
-var element_a = document.getElementsByClassName('anchor-pb');
+var element_a = document.getElementsByClassName("anchor-pb");
 var prev = document.querySelector("div[title='Previous page']");
 var next = document.querySelector("div[title='Next page']");
 prev.style.opacity = 1;
 next.style.opacity = 1;
 prev.addEventListener("click", () => {
-    if (idx == 0) {
-        element_a[idx].scrollIntoView();
-    } else {
-        element_a[prev_idx].scrollIntoView();
-    }
+  if (idx == 0) {
+    element_a[idx].scrollIntoView();
+  } else {
+    element_a[prev_idx].scrollIntoView();
+  }
 });
 next.addEventListener("click", () => {
-    element_a[idx].scrollIntoView();
+  element_a[idx].scrollIntoView();
 });
 
 /*
@@ -157,23 +157,23 @@ function to check if element is close to top of window viewport
 ##################################################################
 */
 function isInViewport(element) {
-    // Get the bounding client rectangle position in the viewport
-    var bounding = element.getBoundingClientRect();
-    // Checking part. Here the code checks if el is close to top of viewport.
-    // console.log("Top");
-    // console.log(bounding.top);
-    // console.log("Bottom");
-    // console.log(bounding.bottom);
-    if (
-        bounding.top <= 180 &&
-        bounding.bottom <= 210 &&
-        bounding.top >= 0 &&
-        bounding.bottom >= 0
-    ) {
-        return true;
-    } else {
-        return false;
-    }
+  // Get the bounding client rectangle position in the viewport
+  var bounding = element.getBoundingClientRect();
+  // Checking part. Here the code checks if el is close to top of viewport.
+  // console.log("Top");
+  // console.log(bounding.top);
+  // console.log("Bottom");
+  // console.log(bounding.bottom);
+  if (
+    bounding.top <= 180 &&
+    bounding.bottom <= 210 &&
+    bounding.top >= 0 &&
+    bounding.bottom >= 0
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /*
@@ -182,25 +182,26 @@ function to check if element is anywhere in window viewport
 ##################################################################
 */
 function isInViewportAll(element) {
-    // Get the bounding client rectangle position in the viewport
-    var bounding = element.getBoundingClientRect();
-    // Checking part. Here the code checks if el is close to top of viewport.
-    // console.log("Top");
-    // console.log(bounding.top);
-    // console.log("Bottom");
-    // console.log(bounding.bottom);
-    if (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    ) {
-        return true;
-    } else {
-        return false;
-    }
+  // Get the bounding client rectangle position in the viewport
+  var bounding = element.getBoundingClientRect();
+  // Checking part. Here the code checks if el is close to top of viewport.
+  // console.log("Top");
+  // console.log(bounding.top);
+  // console.log("Bottom");
+  // console.log(bounding.bottom);
+  if (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 
 /*
 ##################################################################
@@ -208,40 +209,52 @@ eventlisteners to max hight of the container
 ##################################################################
 */
 
-const image_size_relations = container_facs_1.getBoundingClientRect().width / Number(container_facs_1.style.height.replace("px", ""));
+const image_size_relations =
+  container_facs_1.getBoundingClientRect().width /
+  Number(container_facs_1.style.height.replace("px", ""));
 const resize_threshold = window.innerHeight / 3;
 
 function calculate_facsContainer_height(facsContainer) {
-    var image_rights = document.getElementsByClassName("image_rights")[0];
-    var image_rights_height = image_rights.getBoundingClientRect().height;
-    var new_container_height = window.innerHeight - facsContainer.getBoundingClientRect().top - image_rights_height;
-    return new_container_height;
-};
+  var image_rights = document.getElementsByClassName("image_rights")[0];
+  var image_rights_height = image_rights.getBoundingClientRect().height;
+  var new_container_height =
+    window.innerHeight -
+    facsContainer.getBoundingClientRect().top -
+    image_rights_height;
+  return new_container_height;
+}
 
 function calculate_facsContainer_width(new_container_height_number) {
-    return image_size_relations * new_container_height_number;
-};
+  return image_size_relations * new_container_height_number;
+}
 
-function set_facsContainer_hight_and_width(facsContainer, height_number, width_number) {
-    facsContainer.style.height = `${String(height_number)}px`;
-    facsContainer.style.width = `${String(width_number)}px`;
-    console.log(`set height to ${facsContainer.style.height} and width to ${facsContainer.style.width}`)
-};
+function set_facsContainer_hight_and_width(
+  facsContainer,
+  height_number,
+  width_number,
+) {
+  facsContainer.style.height = `${String(height_number)}px`;
+  facsContainer.style.width = `${String(width_number)}px`;
+  console.log(
+    `set height to ${facsContainer.style.height} and width to ${facsContainer.style.width}`,
+  );
+}
 
 function resize_facsContainer() {
-    if ($(document).scrollTop() < resize_threshold) {
-        var container_facs_1 = document.getElementById("container_facs_1");
-        var new_container_height = calculate_facsContainer_height(container_facs_1);
-        var new_container_width = calculate_facsContainer_width(new_container_height);
-        set_facsContainer_hight_and_width(container_facs_1, new_container_height, new_container_width);
-    };
+  if ($(document).scrollTop() < resize_threshold) {
+    var container_facs_1 = document.getElementById("container_facs_1");
+    var new_container_height = calculate_facsContainer_height(container_facs_1);
+    var new_container_width =
+      calculate_facsContainer_width(new_container_height);
+    set_facsContainer_hight_and_width(
+      container_facs_1,
+      new_container_height,
+      new_container_width,
+    );
+  }
 }
 
 var osd_container_resize_events = ["scroll", "resize", "onload"];
 for (trigger_event_type of osd_container_resize_events) {
-        window.addEventListener(trigger_event_type, resize_facsContainer);
-};
-
-/*
-grid columns udn dann dings
-*/
+  window.addEventListener(trigger_event_type, resize_facsContainer);
+}
