0fa57b746a73108004279aa4334286978c8d7f85
cfhaak <c.f.haak@posteo.de>
2023-09-05 11:57:34 +0200

    files now fetched from data repo

--- shellscripts/fetch_editions.sh
+++ shellscripts/fetch_editions.sh
@@ -2,13 +2,13 @@
 # # def vars
 temp_path="./tmp_tmp/"
 zip_file="${temp_path}main.zip"
-fetched_editions_path="${temp_path}bv-working-data-main/data/editions/"
+fetched_editions_path="${temp_path}bv-data-main/data/editions/"
 target_editions_path="./data/editions/"
 # # prepare folder structure
 rm -rf $temp_path
 mkdir $temp_path
 # # fetch data from repo, unzip, and move to destination
-wget https://github.com/bundesverfassung-oesterreich/bv-working-data/archive/refs/heads/main.zip -O $zip_file
+wget https://github.com/bundesverfassung-oesterreich/bv-data/archive/refs/heads/main.zip -O $zip_file
 unzip $zip_file -d $temp_path
 # # check for target dir
 if [ ! -d "$target_editions_path" ]; then mkdir "$target_editions_path"; fi
