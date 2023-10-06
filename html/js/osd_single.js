5143f5bffb397e689db21fe251c51b6600b55fec
cfhaak <c.f.haak@posteo.de>
2023-10-06 14:00:29 +0200

    adjusted version of osd

--- html/js/osd_single.js
+++ html/js/osd_single.js
@@ -8,11 +8,12 @@ function load_image(facs_id, osd_container_id, osd_container_id2){
     var imageURL = {type: 'image', url: image};
     var viewer = OpenSeadragon({
         id: osd_container_id,
-        prefixUrl: 'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/2.4.1/images/',
+        prefixUrl: 'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/4.1.0/images/',
         // sequenceMode: true,
         // showReferenceStrip: true,
         // showNavigator: true,
         // imageLoaderLimit: 10,
+        defaultZoomLevel: 1,
         tileSources: imageURL
     });
     $('#' + osd_container_id2).remove();
