0c91511f877a3e39d765ae0396252a75cf92c8f4
cfhaak <cfhaak@users.noreply.github.com>
2023-11-08 13:51:13 +0000

    deploy: 1e704e5a94d7d5c18197b296d7a5eff73d96c310

--- js/ts_search.js
+++ js/ts_search.js
@@ -48,16 +48,6 @@ search.addWidgets([
           `,
     },
   }),
-  /*
-    removed this from above
-    <div>
-      <div>
-          {{#persons}}
-          <span class="badge bg-secondary">{{ . }}</span>
-          {{/persons}}
-      </div>
-    </div>
-  */
 
   instantsearch.widgets.pagination({
     container: "#pagination",
@@ -185,7 +175,7 @@ search.addWidgets([
   instantsearch.widgets.sortBy({
     container: "#sort-by",
     items: [
-      { label: "Default", value: `${project_collection_name}` },
+      { label: "standard", value: `${project_collection_name}` },
       { label: "chronologisch", value: `${project_collection_name}/sort/creation_date:asc, bv_doc_id_num:asc, doc_internal_orderval:asc` },
       { label: "umgekehrt chronologisch", value: `${project_collection_name}/sort/creation_date:desc, bv_doc_id_num:asc, doc_internal_orderval:asc` },
     ],
