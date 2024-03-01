f94b20923ed71e6021da40e94a303d1f2eecfd29
cfhaak <c.f.haak@posteo.de>
2024-03-01 16:39:12 +0100

    updated autocommit

--- shellscripts/dl_imprint.sh
+++ shellscripts/dl_imprint.sh
@@ -3,7 +3,9 @@
 REDMINE_ID=21497
 IMPRINT_XML=./data/imprint.xml
 rm ${IMPRINT_XML}
-echo '<?xml version="1.0" encoding="UTF-8"?>'
+echo '<?xml version="1.0" encoding="UTF-8"?>' >> ${IMPRINT_XML}
 echo "<root>" >> ${IMPRINT_XML}
-curl https://shared.acdh.oeaw.ac.at/acdh-common-assets/api/imprint.php?serviceID=${REDMINE_ID} >> ${IMPRINT_XML}
+curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}?locale=de >> ${IMPRINT_XML}
+curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}?locale=en >> ${IMPRINT_XML}
+sed -i 's/<br>/<lb\/>/g' $IMPRINT_XML
 echo "</root>" >> ${IMPRINT_XML}
