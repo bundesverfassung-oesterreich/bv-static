1414d779b06d1e7d7d49fb59984c597e5c21b2d0
cfhaak <c.f.haak@posteo.de>
2024-03-08 11:40:32 +0100

    added new Imprint endpoint

--- shellscripts/dl_imprint.sh
+++ shellscripts/dl_imprint.sh
@@ -4,8 +4,6 @@ REDMINE_ID=21497
 IMPRINT_XML=./data/imprint.xml
 rm ${IMPRINT_XML}
 echo '<?xml version="1.0" encoding="UTF-8"?>' >> ${IMPRINT_XML}
-echo "<root>" >> ${IMPRINT_XML}
-curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}?locale=de >> ${IMPRINT_XML}
-curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}?locale=en >> ${IMPRINT_XML}
-sed -i 's/<br>/<lb\/>/g' $IMPRINT_XML
-echo "</root>" >> ${IMPRINT_XML}
+echo "<root><div>" >> ${IMPRINT_XML}
+curl "https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}?locale=de&format=xhtml" >> ${IMPRINT_XML}
+echo "</div></root>" >> ${IMPRINT_XML}
