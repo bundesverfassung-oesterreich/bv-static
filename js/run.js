f484350ffccf0e0c02aedfc5d619367d1296c673
cfhaak <cfhaak@users.noreply.github.com>
2024-05-27 10:42:32 +0000

    deploy: e3db8612299cd70879b53f5a7349c85a689a4c6d

--- js/run.js
+++ js/run.js
@@ -1,125 +1,77 @@
 var editor = new LoadEditor({
-    aot: {
-      title: "Text Annotations",
-      variants: [
-        {
-          opt: "ef",
-          opt_slider: "entities-features-slider",
-          title: "Alles",
-          color: "red",
-          html_class: "undefined",
-          css_class: "undefined",
-          chg_citation: "citation-url",
-          hide: {
-            hidden: false,
-            class: "undefined",
-          },
-          features: {
-            all: true,
-            class: "features-1",
-          },
+  aot: {
+    title: "Text Annotations",
+    variants: [
+      {
+        opt: "ef",
+        opt_slider: "entities-features-slider",
+        title: "Alles",
+        color: "red",
+        html_class: "undefined",
+        css_class: "undefined",
+        chg_citation: "citation-url",
+        hide: {
+          hidden: false,
+          class: "undefined",
         },
-                {
-          opt: "historical_pagecounter",
-          color: "grey",
-          title: "historische Paginierung/Foliierung",
-          html_class: "historical_pagecounter",
-          css_class: "test_class",
-          hide: {
-            hidden: true,
-            class: "historical_pagecounter",
-          },
-          chg_citation: "citation-url",
-          features: {
-            all: false,
-            class: "features-1",
-          },
+        features: {
+          all: true,
+          class: "features-1",
         },
-        {
-          opt: "prs",
-          color: "blue",
-          title: "Personen",
-          html_class: "persons",
-          css_class: "pers",
-          hide: {
-            hidden: false,
-            class: "persons .entity",
-          },
-          chg_citation: "citation-url",
-          features: {
-            all: false,
-            class: "features-1",
-          },
-        },
-        {
-          opt: "org",
-          color: "yellow",
-          //title: "Organizations",
-          title: "Organisationen",
-          html_class: "orgs",
-          css_class: "org",
-          hide: {
-            hidden: false,
-            class: "orgs .entity",
-          },
-          chg_citation: "citation-url",
-          features: {
-            all: false,
-            class: "features-1",
-          },
-        },
-        {
-          opt: "wrk",
-          color: "lila",
-          //title: "Works",
-          title: "Werke",
-          html_class: "works",
-          css_class: "wrk",
-          chg_citation: "citation-url",
-          hide: {
-            hidden: false,
-            class: "wrk .entity",
-          },
-          features: {
-            all: false,
-            class: "features-1",
-          },
-        }
-      ],
-      span_element: {
-        css_class: "badge-item",
       },
-      active_class: "activated",
-      rendered_element: {
-        label_class: "switch",
-        slider_class: "i-slider round",
-      },
-    },
-    fos: {
-      name: "Change font size",
-      variants: [
-        {
-          opt: "fs",
-          //title: "Font Size",
-          title: "Schriftgröße",
-          urlparam: "fs",
-          chg_citation: "citation-url",
-          sizes: {
-            default: "default",
-            font_size_14: "14",
-            font_size_18: "18",
-            font_size_22: "22",
-            font_size_26: "26",
-          },
-          paragraph: ".yes-index",
-          p_class: "",
-          css_class: "font-size-",
+      {
+        opt: "historical_pagecounter",
+        color: "grey",
+        title: "historische Paginierung/Foliierung",
+        html_class: "historical_pagecounter",
+        css_class: "test_class",
+        hide: {
+          hidden: true,
+          class: "historical_pagecounter",
         },
-      ],
-      active_class: "active",
-      html_class: "form-select",
+        chg_citation: "citation-url",
+        features: {
+          all: false,
+          class: "features-1",
+        },
+      },
+    ],
+    span_element: {
+      css_class: "badge-item",
     },
-    wr: false,
-    up: true,
-  });
-  
+    active_class: "active",
+    rendered_element: {
+      label_class: "switch",
+      slider_class: "i-slider round",
+    },
+  },
+  fos: {
+    name: "Change font size",
+    variants: [
+      {
+        // must match opt attribute value of custom element
+        opt: "fos",
+        // visible feature title
+        title: "Font size",
+        // url parameter name
+        urlparam: "fontsize",
+        // custom class for citation link
+        chg_citation: "citation-url",
+        // default font sizes
+        sizes: {
+          default: "default",
+          font_size_14: "14",
+          font_size_18: "18",
+          font_size_22: "22",
+          font_size_26: "26",
+        },
+        // default tag-name containing text
+        paragraph: "div",
+        // default class in combination with paragraph that contains text
+        p_class: "",
+        // default addition to css class. Will be combined with font size value
+        css_class: "font-size-",
+      },
+    ],
+  },
+});
