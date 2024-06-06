203d1547bbfc2f8880df315fb44a7340f50d6a60
cfhaak <c.f.haak@posteo.de>
2024-06-06 13:46:03 +0200

    see #25 basic functions, needs to be more responsive

---
+++ pyscripts/build_xml_docupage.py
@@ -0,0 +1,29 @@
+from lxml import etree
+from lxml import html
+import requests as req
+template_path = "html/index.html"
+docu_path = "html/xml_docu.html"
+
+xml_docu_req = req.get('https://raw.githubusercontent.com/bundesverfassung-oesterreich/bv-schema-framework/main/html/index.html')
+xml_docu_html = html.fromstring(xml_docu_req.content)
+docu_nav_html = xml_docu_html.xpath("//body/ul[@class='toc toc_body']")[0]
+docu_html = xml_docu_html.xpath("//body/div[@class='tei_body']")[0]
+
+with open(template_path, 'rb') as template_file:
+    template_html = html.parse(template_file)
+
+template_container = template_html.xpath("//div[@class='container']")[0]
+template_container.attrib["class"] += " xmldocumentation"
+spacer_div = etree.Element("div")
+spacer_div.attrib["class"] = "xml_docu_spacer"
+if template_container is not None and docu_nav_html is not None and docu_html is not None:
+    for child in template_container:
+        template_container.remove(child)
+    template_container.append(spacer_div)
+    template_container.append(docu_html)
+    template_container.append(docu_nav_html)
+
+
+
+with open(docu_path, 'wb') as output_file:
+    output_file.write(etree.tostring(template_html, pretty_print=True, method="html"))
