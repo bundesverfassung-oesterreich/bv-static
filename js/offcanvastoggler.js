16a47ea35b3dd6f05c4d8a77e34775a9b30f3fd8
cfhaak <cfhaak@users.noreply.github.com>
2024-06-07 11:47:17 +0000

    deploy: 0822852012ee1aa498802fec4ff54f294bbd86d2

--- js/offcanvastoggler.js
+++ js/offcanvastoggler.js
@@ -1,14 +1,16 @@
 function handle_the_handler(off_canvas_id) {
     let offcanvas_container_div = document.getElementById(off_canvas_id);
-    const hideCanvas = (event) => {
-      let openedCanvas = bootstrap.Offcanvas.getInstance(offcanvas_container_div);
-      openedCanvas.hide();
-      event.target.removeEventListener('mouseleave', hideCanvas);
+    if (offcanvas_container_div) {
+      const hideCanvas = (event) => {
+        let openedCanvas = bootstrap.Offcanvas.getInstance(offcanvas_container_div);
+        openedCanvas.hide();
+        event.target.removeEventListener('mouseleave', hideCanvas);
+      }
+      const listenToMouseLeave = (event) => {
+        event.target.addEventListener('mouseleave', hideCanvas);
+      }
+      offcanvas_container_div.addEventListener('shown.bs.offcanvas', listenToMouseLeave);
     }
-    const listenToMouseLeave = (event) => {
-      event.target.addEventListener('mouseleave', hideCanvas);
-    }
-    offcanvas_container_div.addEventListener('shown.bs.offcanvas', listenToMouseLeave);
   }
 
 
