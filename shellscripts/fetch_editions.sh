439c923537a48d53e2810fe1493519ecd639e7bb
cfhaak <c.f.haak@posteo.de>
2024-09-11 12:47:03 +0200

    see #155; commentaries get fetched

--- shellscripts/fetch_editions.sh
+++ shellscripts/fetch_editions.sh
@@ -3,7 +3,9 @@
 temp_path="./tmp_tmp/"
 zip_file="${temp_path}main.zip"
 fetched_editions_path="${temp_path}bv-data-main/data/editions/"
+fetched_commentaries_path="${temp_path}bv-data-main/data/commentaries/"
 target_editions_path="./data/editions/"
+target_commentaries_path="./data/commentaries/"
 # # prepare folder structure
 rm -rf $temp_path
 mkdir $temp_path
@@ -12,6 +14,8 @@ wget https://github.com/bundesverfassung-oesterreich/bv-data/archive/refs/heads/
 unzip $zip_file -d $temp_path
 # # check for target dir
 if [ ! -d "$target_editions_path" ]; then mkdir "$target_editions_path"; fi
+if [ ! -d "$target_commentaries_path" ]; then mkdir "$target_commentaries_path"; fi
 find $fetched_editions_path -name "*.xml" -exec cp "{}" "$target_editions_path" \;
+find $fetched_commentaries_path -name "*.xml" -exec cp "{}" "$target_commentaries_path" \;
 # # remove temp folder
 rm -rf $temp_path
