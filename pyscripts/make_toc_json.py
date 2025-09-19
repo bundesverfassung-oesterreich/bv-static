7a6b8c9f3c8aa67f3e2f2ecf818f393d5ba4763e
cfhaak <c.f.haak@posteo.de>
2025-09-19 13:28:11 +0200

    changed default order to fit new constrains

--- pyscripts/make_toc_json.py
+++ pyscripts/make_toc_json.py
@@ -8,11 +8,13 @@ editions_path = "./data/editions/*.xml"
 doc_id_2_data = {}
 toc = []
 
+
 def normalize_space(text):
     """
     Normalize whitespace in a string by replacing multiple spaces and newlines with a single space.
     """
     return re.sub(r'\s+', ' ', text).strip()
+
 
 for edition_file in glob.glob(editions_path):
     print(edition_file)
@@ -44,9 +46,12 @@ for edition_file in glob.glob(editions_path):
     data["Materialtyp"] = doc.any_xpath(
         "//tei:sourceDesc/tei:msDesc/tei:physDesc/tei:objectDesc/@form[1]"
     )[0]
-    data["Entstehung (tpq)"] = doc.any_xpath(
+    date_val = doc.any_xpath(
         "//tei:profileDesc/tei:creation/tei:date/@notBefore-iso[1]"
+    )[0].strip() + "|" + doc.any_xpath(
+        "//tei:seriesStmt/tei:biblScope/text()"
     )[0].strip()
+    data["Entstehung (tpq)"] = date_val
     revision_desc = doc.any_xpath(".//tei:revisionDesc/@status")[0].strip()
     revision_state = "maschinell erfasst"
     match revision_desc:
@@ -58,9 +63,10 @@ for edition_file in glob.glob(editions_path):
             revision_state = "vollständig ediert"
         case _:
             pass  # keep default 'maschinell erfasst'
+
     data["Erschließungsgrad"] = revision_state
     bv_doc_id = doc.tree.getroot().attrib[f'{{{doc.ns_xml.get("xml")}}}id']
-    
+
     doc_id_2_data[bv_doc_id] = data
     if data["witness_status"] == "primary":
         toc.append(data)
@@ -77,6 +83,11 @@ for data in toc:
             # we have to remove this, otherwise tabulator will show a non working dropdown
             data.pop("_children")
             data.pop("other_witnesses")
+
+# sort by:
+# 1. Data Set (A-C)
+# 2. Entstehung (tpq), split by "|" and sort by first part (date), then by second part (biblScope)
+toc = sorted(toc, key=lambda x: (x["data_set"], x["Entstehung (tpq)"].split("|")[0], x["Entstehung (tpq)"].split("|")[1]))
 with open(json_path, "w") as f:
     print(f"Writing toc to {json_path}")
     json.dump(toc, f, indent=2)
